8c542a81187a5919939ee87a26b78e8cf7c77878
ZawojWeb <79516803+ZawojWeb@users.noreply.github.com>
2021-06-06 20:13:43 +0200

    Navigacja

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 // Global Stlye
 import GlobalStyle from './components/GlobalStyle'
 import AboutUsPage from './pages/AboutUsPage'
+import Nav from './components/Nav'
 
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle></GlobalStyle>
+      <Nav></Nav>
       <AboutUsPage></AboutUsPage>
     </div>
   );
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,9 +3,41 @@ import styled from 'styled-components'
 
 const Nav = () => {
     return(
-        <nav>
-
-        </nav>
+        <SNav>
+            <h1 id="logo"><a href="#">Portfolio Demo</a></h1>
+            <ul>
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">Our Work</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </SNav>
     )
 }
+
+const SNav = styled.nav`
+min-height: 10vh;
+display: flex;
+margin: auto;
+justify-content: space-between;
+align-items: center;
+padding: 1rem 5rem;
+background: #282828;
+a{
+    text-decoration: none;
+    color: white;
+}
+ul{
+    display: flex;
+    list-style: none;
+    li{
+        padding-left: 10rem;
+        position: relative;
+    }
+}
+#logo{
+    font-size: 1.8rem;
+    font-family: 'Lobster',cursive;
+    font-weight: lighter;
+}
+`
 export default Nav
