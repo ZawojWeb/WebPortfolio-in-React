f987eb85ff8e4586cc5b30d7e8f213c56c07a30e
ZawojWeb <zawojweb@gmail.com>
2021-06-07 14:44:09 +0200

    Animation more

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,31 +1,29 @@
 import React from 'react';
 import home1 from "../img/home1.png";
 import {SAbout,SDesc,SImage,SHideDiv} from '../styles'
-
 // Framer Motion
 import {motion} from 'framer-motion'
+import {titleAnim, fade, photoAnim} from '../animation'
 const AboutSection = () => {
-
-
     return(
         <SAbout>
             <SDesc>
                 <motion.header>
                     <SHideDiv>
-                        <motion.h2>We Work to make</motion.h2>
+                        <motion.h2 variants={titleAnim} >We Work to make</motion.h2>
                     </SHideDiv>
                     <SHideDiv>
-                        <motion.h2>Your <span>dreams</span></motion.h2>
+                        <motion.h2 variants={titleAnim} >Your <span>dreams</span></motion.h2>
                     </SHideDiv>
                     <SHideDiv>
-                        <motion.h2>come true.</motion.h2>
+                        <motion.h2 variants={titleAnim}>come true.</motion.h2>
                     </SHideDiv>
                 </motion.header>
-                <p>Contact us for any photography or video ideas taht you have. We have professional with amazing skills.</p>
-                <button>Contact Us</button>
+                <motion.p variants={fade}>Contact us for any photography or video ideas taht you have. We have professional with amazing skills.</motion.p>
+                <motion.button variants={fade}>Contact Us</motion.button>
             </SDesc>
             <SImage>
-                <img src={home1} alt="guy with camera" />
+                <motion.img variants={photoAnim} src={home1} alt="guy with camera" />
             </SImage>
         </SAbout>
     );
